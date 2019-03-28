80b25430d74364cd1caedcc3ffebad5931f8220e
Patrick Emond <emondpd@mcmaster.ca>
2019-03-28 15:05:23 -0400

    First commit

---
+++ src/exception/error_codes.inc.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * error_codes.inc.php
+ * 
+ * This file is where all error codes are defined.
+ * All error code are named after the class and function they occur in.
+ */
+
+/**
+ * Error number category defines.
+ */
+define( 'ARGUMENT_CENOZAPP_BASE_ERRNO',   110000 );
+define( 'DATABASE_CENOZAPP_BASE_ERRNO',   210000 );
+define( 'LDAP_CENOZAPP_BASE_ERRNO',       310000 );
+define( 'NOTICE_CENOZAPP_BASE_ERRNO',     410000 );
+define( 'PERMISSION_CENOZAPP_BASE_ERRNO', 510000 );
+define( 'RUNTIME_CENOZAPP_BASE_ERRNO',    610000 );
+define( 'SYSTEM_CENOZAPP_BASE_ERRNO',     710000 );
+
+/**
+ * "argument" error codes
+ */
+
+/**
+ * "database" error codes
+ * 
+ * Since database errors already have codes this list is likely to stay empty.
+ */
+
+/**
+ * "ldap" error codes
+ * 
+ * Since ldap errors already have codes this list is likely to stay empty.
+ */
+
+/**
+ * "notice" error codes
+ */
+
+/**
+ * "permission" error codes
+ */
+
+/**
+ * "runtime" error codes
+ */
+
+/**
+ * "system" error codes
+ * 
+ * Since system errors already have codes this list is likely to stay empty.
+ * Note the following PHP error codes:
+ *      1: error,
+ *      2: warning,
+ *      4: parse,
+ *      8: notice,
+ *     16: core error,
+ *     32: core warning,
+ *     64: compile error,
+ *    128: compile warning,
+ *    256: user error,
+ *    512: user warning,
+ *   1024: user notice
+ */
+
